c8828a512b2b06c62c7205eb5d44e9f6310ebf9f
likhith <likhithaningi@gmail.com>
2024-05-01 01:28:54 +0530

    test

---
+++ index.js
@@ -0,0 +1,81 @@
+document.getElementById("year").innerText = new Date().getFullYear();
+
+function closeForm() {
+  var form = document.getElementById("contactForm");
+  form.classList.remove("fadeIn");
+  form.classList.add("fadeOut");
+  setTimeout(function () {
+    form.style.display = "none";
+  }, 370);
+}
+function toggleForm() {
+  var form = document.getElementById("contactForm");
+  if (form.style.display === "none" || form.style.display === "") {
+    form.style.display = "block";
+    form.classList.remove("fadeOut");
+    form.classList.add("fadeIn");
+  } else {
+    form.classList.remove("fadeIn");
+    form.classList.add("fadeOut");
+    setTimeout(function () {
+      form.style.display = "none";
+    }, 370);
+  }
+}
+
+document.addEventListener("click", function (event) {
+  var form = document.getElementById("contactForm");
+  var button = document.querySelector(".contact-button");
+  if (
+    event.target !== form &&
+    event.target !== button &&
+    !form.contains(event.target)
+  ) {
+    form.classList.remove("fadeIn");
+    form.classList.add("fadeOut");
+    setTimeout(function () {
+      form.style.display = "none";
+    }, 370);
+  }
+});
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  const formData = new FormData(form);
+  e.preventDefault();
+
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = json.message;
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+        closeForm();
+      }, 3000);
+    });
+});
